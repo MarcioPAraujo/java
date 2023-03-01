0dc1464d97bbb49ec222bf1a3b8f2b5074b896b7
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-03-01 19:55:45 -0300

    negative delta is no more accepted

--- college/FuncaoQuadratica/src/App.java
+++ college/FuncaoQuadratica/src/App.java
@@ -1,21 +1,31 @@
 import java.util.Random;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
         
         Funcao funcao = new Funcao();
         Random random = new Random();
+        calculate(funcao,random);
+        funcao.calcularDelta();
+        if(funcao.getDelta()<0){
+            calculate(funcao,random);
+            funcao.calcularDelta();
+        }
+        else{
+            funcao.calcularXUm();
+            funcao.calcularXDois();
+
+            System.out.println("o valor de delta: "+ funcao.getDelta());
+            System.out.println("o valor de x1: "+ funcao.getXUm());
+            System.out.println("o valor de x2: "+ funcao.getXDois());
+        }
+    }
+    public static void calculate(Funcao funcao,Random random){
+        
+        
         funcao.setA(random.nextDouble(100)+1);
         funcao.setB(random.nextDouble(100)+1);
         funcao.setC(random.nextDouble(100)+1);
-
-        funcao.calcularDelta();
-        
-        funcao.calcularXUm();
-        funcao.calcularXDois();
-
-        System.out.println("o valor de delta: "+ funcao.getDelta());
-        System.out.println("o valor de x1: "+ funcao.getXUm());
-        System.out.println("o valor de x2: "+ funcao.getXDois());
     }
 }
