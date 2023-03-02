3870971f1ebbaca18dfc72bdc7d016a2edb39fad
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-03-01 21:43:18 -0300

    exercice conclued

--- college/EquacaoSegundoGrau/src/App.java
+++ college/EquacaoSegundoGrau/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Equacao equacao = new Equacao();
+        equacao.setA(1);
+        equacao.setB(-5);
+        equacao.setC(6);
+
+        equacao.calculateDelta();
+        equacao.calculateX1();
+        equacao.calculateX2();
+
+        System.out.println("x 1: "+equacao.getX1());
+        System.out.println("x 2: "+equacao.getX2());
     }
 }
