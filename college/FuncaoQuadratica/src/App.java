b22deff4a73b7f2751da43faa2fbb21e1fc9b0f5
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-03-01 21:17:16 -0300

    delta validation was added

--- college/FuncaoQuadratica/src/App.java
+++ college/FuncaoQuadratica/src/App.java
@@ -7,19 +7,24 @@ public class App {
         Funcao funcao = new Funcao();
         Random random = new Random();
         calculate(funcao,random);
-        funcao.calcularDelta();
-        if(funcao.getDelta()<0){
-            calculate(funcao,random);
-            funcao.calcularDelta();
-        }
-        else{
-            funcao.calcularXUm();
-            funcao.calcularXDois();
+        boolean key = false;
+        while(key==false){
+            
+            key = validateDelta(funcao,random);
+            if(key==false){
+                calculate(funcao,random);
+            }
 
-            System.out.println("o valor de delta: "+ funcao.getDelta());
-            System.out.println("o valor de x1: "+ funcao.getXUm());
-            System.out.println("o valor de x2: "+ funcao.getXDois());
         }
+        
+        
+        funcao.calcularXUm();
+        funcao.calcularXDois();
+
+        System.out.println("o valor de delta: "+ funcao.getDelta());
+        System.out.println("o valor de x1: "+ funcao.getXUm());
+        System.out.println("o valor de x2: "+ funcao.getXDois());
+        
     }
     public static void calculate(Funcao funcao,Random random){
         
@@ -27,5 +32,15 @@ public class App {
         funcao.setA(random.nextDouble(100)+1);
         funcao.setB(random.nextDouble(100)+1);
         funcao.setC(random.nextDouble(100)+1);
+    }
+
+    public static boolean validateDelta(Funcao funcao,Random random){
+        funcao.calcularDelta();
+        if(funcao.getDelta()<0){
+           return false;
+        }
+        else{
+            return true;
+        }
     }
 }
