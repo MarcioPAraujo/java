ac31ea60319a4c1f5ac7e3ec2172d3d62683cecd
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-03-21 07:27:15 -0300

    Update LocacaoDeQuadra.java

--- college/LocacaoDeQuadra/src/LocacaoDeQuadra.java
+++ college/LocacaoDeQuadra/src/LocacaoDeQuadra.java
@@ -1,8 +1,8 @@
-import javax.swing.JOptionPane;
+
 
 public class LocacaoDeQuadra {
     static void main(String[] args) throws Exception {
-        JOptionPane.showMessageDialog(null, "Hello I want to play a game");
+    
 
         Locacao novaLocacao = new Locacao();
         novaLocacao.cadastrarLocacao();
