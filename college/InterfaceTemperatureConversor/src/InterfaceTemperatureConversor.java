3f15b5d69964beb76ffbf00f59ea5236f126096b
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-04-25 21:09:19 -0300

    frame was created

---
+++ college/InterfaceTemperatureConversor/src/InterfaceTemperatureConversor.java
@@ -0,0 +1,18 @@
+package appConversor;
+
+import javax.swing.*;
+
+public class InterfaceTemperatureConversor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//JOptionPane.showMessageDialog(null,"hello jeff!!");
+		JFrame frame = new JFrame();
+		frame.setTitle("Temperature Conversor");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		
+	}
+
+}
