938b4263bdcd1de62f608296b54fd68998cc9992
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-05-01 17:05:01 -0300

    Introduction to JFrame

---
+++ introduction to jframe/src/Introduction.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame screen = new JFrame();
+		screen.setVisible(true);
+		screen.setSize(600,600);
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.setTitle("jframe intoduction");
+		
+		
+		
+		
+	}
+
+}
