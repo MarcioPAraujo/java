da4385da85cffbd7c811a72ee27178272756bb1c
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2023-05-01 17:48:26 -0300

    using a class to make a frame

--- introduction to jframe/src/Introduction.java
+++ introduction to jframe/src/Introduction.java
@@ -7,14 +7,15 @@ public class Introduction {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame screen = new JFrame();
+		/*JFrame screen = new JFrame();
 		screen.setVisible(true);
 		screen.setSize(600,600);
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		screen.setTitle("jframe intoduction");
 		
 		screen.getContentPane().setBackground(Color.pink);
-		
+		*/
+		MyFrame screen = new MyFrame();
 		
 		
 	}
